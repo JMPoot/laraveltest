f26171a5b3602b64b502c80803d6f74beaff7581
JMPoot <justinpoot@live.nl>
2017-09-13 15:35:26 +0200

    markup and added my first facade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,33 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <h1>some content</h1>
-            @if(false)
-                <p>This only displays if it is true</p>
-            @else
-                <p>This only displays if it is false</p>
-            @endif
-            <hr>
-            @for($i = 0; $i < 5; $i++)
-                <p>{{ $i + 1}}. Iteration</p>
-            @endfor
+            <div class="quote">
+                The Beautiful Laravel
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">Learning Laravel</h1>
+            <p>This blog post will get you right on track with Laravel!</p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">The next steps</h1>
+            <p>Understanding the Basisc is great, but you need to be able to make the next steps.</p>
+            <p><a href="#">Read more...</a></p>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1 class="post-title">Laravel 5.3</h1>
+            <p>Though announced as a "minor release", Laravel 5.3 ships with some very interesting additions and
+            features</p>
+            <p><a href="#">Read more...</a></p>
         </div>
     </div>
 @endsection
