ab4ede5dec1e1b32d4089de0d52ea1d1b316b7b5
JMPoot <justinpoot@live.nl>
2017-09-24 22:07:46 +0200

    Data is now getting fetched from the Database instead of sessions

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,20 +10,17 @@ use Illuminate\Session\Store;
 class PostController extends Controller
 {
     public function getIndex(Store $session) {
-        $post = new Post();
-        $posts = $post->getPosts($session);
+        $posts = Post::all();
         return view('blog.index', ['posts' => $posts]);
     }
 
     public function getAdminIndex(Store $session) {
-        $post = new Post();
-        $posts = $post->getPosts($session);
+        $posts = Post::all();
         return view('admin.index', ['posts' => $posts]);
     }
 
     public function getPost(Store $session, $id) {
-        $post = new Post();
-        $post = $post->getPost($session, $id);
+        $post = Post::find($id);
         return view('blog.post', ['post' => $post]);
     }
 
@@ -36,15 +33,19 @@ class PostController extends Controller
             'title' => 'required|min:5',
             'content' => 'required|min:10'
         ]);
-        $post = new Post();
-        $post->addPost($session, $request->input('title'), $request->input('content'));
-        return redirect()->route('admin.index')->with('info', 'Post edited, new Title is :'
+
+        $post = new Post([
+            'title' => $request->input('title'),
+            'content' => $request->input('content')
+        ]);
+        $post->save();
+
+        return redirect()->route('admin.index')->with('info', 'Post edited, new Title is : '
             . $request->input('title'));
     }
 
     public function getAdminEdit(Store $session, $id) {
-        $post = new Post();
-        $post = $post->getPost($session, $id);
+        $post = Post::find($id);
         return view('admin.edit', ['post' => $post, 'postId' => $id]);
     }
 
